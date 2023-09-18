23cf6711ab37815274356420d5f3668d4712015a
Mariela Cejas <marualecejas@hotmail.com>
2023-09-18 11:08:20 -0300

    aca primer commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import NavBar from './components/NavBar'
+import ItemListContainer from './components/ItemListConteiner'
+
+function App() {
+
+
+  return (
+    <div>
+      <NavBar />
+      <ItemListContainer greeting= "En que podemos ayudarte??" />
+  
+            
+            <h1>Detailing</h1>
+            <h3>En Casa!!</h3>
+
+    </div>
+  )
+}
+
+export default App
